6c8d2f86b212bb7430016b762f9c69fc877491a7
Toddlesplash001 <118872908+Toddlesplash001@users.noreply.github.com>
2022-11-23 10:10:18 +0530

    Add files via upload

---
+++ Hello World .py
@@ -0,0 +1,9 @@
+a = int (input ("first integer\n"))
+b = int (input ("second integer\n"))
+c = int (input ("third integer\n"))
+if a>b and a>b:
+    print ("a is greatest")
+if b>a and b>c:
+    print ("b is greatest")
+else:
+    print ("c is greatest")
